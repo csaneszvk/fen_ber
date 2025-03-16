8fefb4ef42e2f2d9c25c60be959e42a5af72333d
Csanadd <fenecsanad@gmail.com>
2025-03-16 17:47:31 +0100

    javítás 3

--- launch/ virag_launch.py
+++ launch/ virag_launch.py
@@ -5,12 +5,16 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='turtlesim', 
-            executable='turtlesim_node', 
+             package='turtlesim', 
+             executable='turtlesim_node', 
+             name='turtlesim',  
+             output='screen' 
         ),
         
         Node(
-            package='fen_ber',  
-            executable='cmd_gen_node', 
+             package='fen_ber',  
+             executable='cmd_gen_node', 
+             name='cmd_gen_node',  
+             output='screen' 
         )
     ])
