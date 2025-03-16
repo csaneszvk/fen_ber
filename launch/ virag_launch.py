13a0973ac040e2561e45d200cf0bcfaec0ccde1f
Csanadd <fenecsanad@gmail.com>
2025-03-16 18:08:21 +0100

    Revert "javítás 3"
    This reverts commit 8fefb4ef42e2f2d9c25c60be959e42a5af72333d.

--- launch/ virag_launch.py
+++ launch/ virag_launch.py
@@ -5,16 +5,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-             package='turtlesim', 
-             executable='turtlesim_node', 
-             name='turtlesim',  
-             output='screen' 
+            package='turtlesim', 
+            executable='turtlesim_node', 
         ),
         
         Node(
-             package='fen_ber',  
-             executable='cmd_gen_node', 
-             name='cmd_gen_node',  
-             output='screen' 
+            package='fen_ber',  
+            executable='cmd_gen_node', 
         )
     ])
