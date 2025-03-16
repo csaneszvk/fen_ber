befb1068095abc20b38fc3b155cb8c43d7f6c411
Csanadd <fenecsanad@gmail.com>
2025-03-16 17:21:08 +0100

    javítás setupban

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 from glob import glob
 import os
 
-package_name = 'fen_ber'
+package_name = 'bag_njb'
 
 setup(
     name=package_name,
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/launch.[pxy][yma]')), 
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), 
     ],
     install_requires=['setuptools', 'rclpy', 'geometry_msgs', 'turtlesim'],
     zip_safe=True,
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'cmd_gen_node = cmd_gen_node:main',
+            'cmd_gen_node.py = fen_ber.cmd_gen_node:main',
         ],
     },
 )
